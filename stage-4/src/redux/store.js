bfb997b3b14933e72f814b5d5688949ed5a75d9c
Rachel Hicken <rachkh22@gmail.com>
2018-05-16 15:00:51 -0600

    fixed a few bugs stage-4

--- stage-4/src/redux/store.js
+++ stage-4/src/redux/store.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import reduxPromiseMiddleware from 'redux-promise-middleware';
 
-export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
+export default createStore(reducer);
